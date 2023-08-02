7f07d779755585a8fa39509c36210af646caa0fb
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-08-02 20:31:47 -0300

    access token changes

--- src/app/contexts/AuthContext.tsx
+++ src/app/contexts/AuthContext.tsx
@@ -1,9 +1,14 @@
-import { createContext, useCallback, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 import { localStorageKeys } from "../config/localStorageKeys";
+import { useQuery } from "@tanstack/react-query";
+import { usersService } from "../services/usersService";
+import toast from "react-hot-toast";
+
 
 interface AuthContextValue {
     signedIn: boolean;
     signin(accessToken: string): void;
+    signout(): void;
 }
 
 export const AuthContext = createContext<AuthContextValue>({} as AuthContextValue);
@@ -14,16 +19,36 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     
         return !!storedAccessToken;
     });
-    
+
+
+    const { isError } = useQuery({
+        queryKey: ['users', 'me'],
+        queryFn: () => usersService.me(),
+        enabled: signedIn,
+    });
+
     const signin = useCallback((accessToken: string) => {
         localStorage.setItem(localStorageKeys.ACCESS_TOKEN, accessToken);
-        
-        
+
         setSignedIn(true);
     }, []);
     
+
+    const signout = useCallback(() => {
+        localStorage.removeItem(localStorageKeys.ACCESS_TOKEN)
+        setSignedIn(false);
+    }, [])
+
+    useEffect(() => {
+        if (isError) {
+            toast.error('Sua sessão expirou');
+            signout();
+        }
+    }, [isError, signout])
+
+
     return (
-        <AuthContext.Provider value={{signedIn, signin}}>
+        <AuthContext.Provider value={{signedIn, signin, signout}}>
             {children}
         </AuthContext.Provider>
     );
--- src/view/pages/Dashboard/index.tsx
+++ src/view/pages/Dashboard/index.tsx
@@ -1,5 +1,13 @@
+import { useAuth } from "../../../app/hooks/useAuth";
+import { Button } from "../../components/Button";
+
 export function Dashboard() {
+    const { signout } = useAuth();
     return (
+        <div>
         <h1>Dashboard Page</h1>
+
+        <Button onClick={signout}>Sair</Button>
+        </div>
     );
 }
