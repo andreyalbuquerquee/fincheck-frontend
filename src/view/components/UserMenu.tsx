7cc82ec11f4b9a1339f6480a30c4b2409a72f9f9
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-08-03 18:34:04 -0300

    UserMenu

---
+++ src/view/components/UserMenu.tsx
@@ -0,0 +1,7 @@
+export function UserMenu() {
+    return (
+        <div className="bg-teal-50 rounded-full w-12 h-12 flex justify-center items-center border border-teal-100">
+            <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">AB</span>
+        </div>
+    )
+}
