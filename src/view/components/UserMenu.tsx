0a422dd67f481187cfcecd60397c478dc1997667
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-08-08 12:28:00 -0300

    Starting dropdown menu

--- src/view/components/UserMenu.tsx
+++ src/view/components/UserMenu.tsx
@@ -1,7 +1,20 @@
+import { DropdownMenu } from "./DropdownMenu";
+
 export function UserMenu() {
     return (
+        <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
         <div className="bg-teal-50 rounded-full w-12 h-12 flex justify-center items-center border border-teal-100">
             <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">AB</span>
         </div>
+        </DropdownMenu.Trigger>
+
+        <DropdownMenu.Content>
+        <DropdownMenu.Item>
+                Sair
+        </DropdownMenu.Item>
+        </DropdownMenu.Content>
+
+        </DropdownMenu.Root>
     )
 }
