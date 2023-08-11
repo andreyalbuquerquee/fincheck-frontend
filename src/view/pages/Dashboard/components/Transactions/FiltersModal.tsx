402942acd0abbeeb4668aee9851f8ac191be5e53
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-08-11 19:58:56 -0300

    Accounts Modal

--- src/view/pages/Dashboard/components/Transactions/FiltersModal.tsx
+++ src/view/pages/Dashboard/components/Transactions/FiltersModal.tsx
@@ -9,7 +9,14 @@ interface FiltersModalProps {
 export function FiltersModal({ open, onClose }: FiltersModalProps) {
     return (
         <Modal open={open} onClose={onClose} title="Filtros">
-            Filtros...
+            <div>
+            <span className="text-lg tracking-[-1px] font-bold text-gray-800">Conta</span>
+            </div>
+            <div className="space-y-2 mt-2">
+                <button className="p-2 rounded-2xl w-full text-left text-gray-800 bg-gray-200">Nubank</button>
+                <button className="p-2 rounded-2xl w-full text-left text-gray-800">XP Investimentos</button>
+                <button className="p-2 rounded-2xl w-full text-left text-gray-800">Dinheiro</button>
+            </div>
         </Modal>
     );
 }
