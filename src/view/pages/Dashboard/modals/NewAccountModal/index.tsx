3c4b87e5066067174d69bb560419f7dac5e70966
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-08-13 17:21:32 -0300

    finished new account modal

--- src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
+++ src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "../../../../components/Button";
 import { ColorsDropdownInput } from "../../../../components/ColorsDropdownInput";
 import { Input } from "../../../../components/Input";
 import { InputCurrency } from "../../../../components/InputCurrency";
@@ -29,6 +30,11 @@ export function NewAccountModal() {
 
                     <ColorsDropdownInput/>
                 </div>
+
+                <Button type="submit" className="w-full mt-6">
+                    Criar
+                </Button>
+
             </form>
         </Modal>
     );
